c977b3e48ed776997d547a746333dc13549cc14b
jbbarth <jeanbaptiste.barth@gmail.com>
2014-02-21 09:34:11 +0100

    Avoid some error in tests: ActionView::Template::Error: uninitialized constant PatchingProjectModel::IssueTemplate

--- lib/redmine_templates/redmine_core_patch.rb
+++ lib/redmine_templates/redmine_core_patch.rb
@@ -1,3 +1,6 @@
+#for some reasons this directory isn't added in the path when we need it
+require_dependency File.expand_path('../../../app/models/issue_template', __FILE__)
+
 module PatchingProjectModel
   def get_issue_templates
     IssueTemplate.includes(:projects).where("projects.id" => self.id ).order("tracker_id")
