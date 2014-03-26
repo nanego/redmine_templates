4d5ab4deb6839e7d84e1598e0627dd31e6d723db
jbbarth <jeanbaptiste.barth@gmail.com>
2014-03-26 15:01:33 +0100

    Add missing fixtures declarations that prevent launching integration tests separately from unit/functional ones

--- test/integration/issue_templates_test.rb
+++ test/integration/issue_templates_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class IssueTemplatesTest < ActionController::IntegrationTest
-  fixtures :users
+  fixtures :users, :projects, :trackers, :issue_statuses
 
   # create a template
   def test_add_issue_template
