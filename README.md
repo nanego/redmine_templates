04870acf987f441293649e1603cecaf78c960e53
Vincent Robert <vincent.robert@nanego.com>
2014-04-02 11:46:59 +0200

    Specify plugin dependency

---
+++ README.md
@@ -0,0 +1,7 @@
+Redmine Issue Templates Plugin
+======================
+
+This plugin adds the ability to create and use issue templates.
+You can manage templates generation and visibility through roles, permissions and projects.
+
+Note that this plugin now depends on [redmine_base_select2](https://github.com/jbbarth/redmine_base_select2).
--- init.rb
+++ init.rb
@@ -17,6 +17,7 @@ Redmine::Plugin.register :redmine_templates do
   version '0.1'
   url 'https://github.com/nanego/redmine_issue_templates'
   requires_redmine :version_or_higher => '2.2.0'
+  requires_redmine_plugin :redmine_base_select2, :version_or_higher => '0.0.1'
   permission :create_issue_templates, {:issue_templates => [:init, :new, :create, :edit, :update, :index, :destroy]}
   settings :default => { 'custom_fields' => [], 'disable_templates' => false},
            :partial => 'settings/redmine_plugin_templates_settings'
