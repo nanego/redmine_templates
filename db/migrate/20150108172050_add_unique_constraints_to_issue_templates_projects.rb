4925a8e27925a27ebe20f94d0925a98a25e8467e
Vincent Robert <vincent.robert@nanego.com>
2015-01-08 17:39:14 +0100

    Add unique constraint to issue template project

---
+++ db/migrate/20150108172050_add_unique_constraints_to_issue_templates_projects.rb
@@ -0,0 +1,15 @@
+class AddUniqueConstraintsToIssueTemplatesProjects < ActiveRecord::Migration
+  def self.up
+    IssueTemplate.all.each do |template|
+      projects = template.projects.uniq
+      template.projects = []
+      template.projects = projects
+      template.save
+    end
+    add_index :issue_templates_projects, ["project_id", "issue_template_id"], :name=> :unique_issue_template_project , :unique => true
+  end
+
+  def self.down
+    remove_index :issue_templates_projects, :name=> :unique_issue_template_project
+  end
+end
