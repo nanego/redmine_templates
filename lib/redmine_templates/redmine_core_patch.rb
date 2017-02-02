0b12e8ecab44675971ed649a20cda65ba5635eff
Vincent Robert <vincent.robert@nanego.com>
2017-02-02 14:50:25 +0100

    Update templates each time a project is updated

--- lib/redmine_templates/redmine_core_patch.rb
+++ lib/redmine_templates/redmine_core_patch.rb
@@ -2,14 +2,30 @@
 require_dependency File.expand_path('../../../app/models/issue_template', __FILE__)
 
 module PatchingProjectModel
+
   def get_issue_templates
     IssueTemplate.includes(:projects).where("projects.id" => self.id ).order("tracker_id")
   end
+
   def get_activated_issue_templates
     get_issue_templates.where.not("issue_templates.id" => self.issue_template_exclusions.map(&:issue_template_id))
   end
+
+  def update_all_issue_templates
+    IssueTemplate.all.each do |template|
+      template.update_projects_through_custom_fields
+      template.save
+    end
+  end
+
 end
+
 Project.class_eval do
+
   has_many :issue_template_exclusions, dependent: :destroy
+
+  after_save :update_all_issue_templates
+
   include PatchingProjectModel
+
 end
