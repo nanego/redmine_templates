c525c9d83fecd20db1fbafeefc13fa573d5b4757
Vincent Robert <vincent.robert@nanego.com>
2017-02-14 09:58:18 +0100

    Skip database creation if already exist

--- db/migrate/20170131084952_create_issue_template_exclusions.rb
+++ db/migrate/20170131084952_create_issue_template_exclusions.rb
@@ -1,11 +1,13 @@
 class CreateIssueTemplateExclusions < ActiveRecord::Migration
   def change
-    create_table :issue_template_exclusions, :id => false do |t|
-      t.belongs_to :issue_template
-      t.belongs_to :project
+    unless ActiveRecord::Base.connection.table_exists? :issue_template_exclusions
+      create_table :issue_template_exclusions, :id => false do |t|
+        t.belongs_to :issue_template
+        t.belongs_to :project
+      end
+      add_index :issue_template_exclusions, ["project_id", "issue_template_id"], :name=> :unique_issue_template_exclusion_per_project, :unique => true
+      add_index :issue_template_exclusions, :issue_template_id
+      add_index :issue_template_exclusions, :project_id
     end
-    add_index :issue_template_exclusions, ["project_id", "issue_template_id"], :name=> :unique_issue_template_exclusion_per_project, :unique => true
-    add_index :issue_template_exclusions, :issue_template_id
-    add_index :issue_template_exclusions, :project_id
   end
 end
