8880943fef03aa8dfcb80e2efa3ee044f13458ae
Vincent Robert <vincent.robert@nanego.com>
2019-07-11 10:51:51 +0200

    Always refresh templates names in menu

--- lib/redmine_templates/menu_manager_patch.rb
+++ lib/redmine_templates/menu_manager_patch.rb
@@ -6,13 +6,13 @@ module Redmine
         ### START PATCH
         if project.present? && project.issue_templates.present?
           Redmine::MenuManager.map :project_menu do |project_menu|
-            project.issue_templates.each do |template|
+            project.issue_templates.includes(:tracker).order('trackers.position desc, issue_templates.template_title desc').each do |template|
               puts "project_menu.find(:new_issue_template_#{template.id}) : #{project_menu.find("new_issue_template_#{template.id}".to_sym).inspect}"
               unless project_menu.find("new_issue_template_#{template.id}".to_sym)
                 project_menu.push "new_issue_template_#{template.id}".to_sym,
                                   { :controller => 'issues', :action => 'new', :template_id => template.id },
                                   :param => :project_id,
-                                  :caption => "[#{template.tracker}] #{template.template_title}",
+                                  :caption => Proc.new {|p| template.reload; "[#{template.tracker}] #{template.template_title}" },
                                   :html => {:accesskey => Redmine::AccessKeys.key_for(:new_issue)},
                                   :if => Proc.new {|p| Issue.allowed_target_trackers(p).any? &&
                                       template.projects.include?(p)},
