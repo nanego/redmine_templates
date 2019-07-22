23495f62e32615538ece36147c0557a6d50e48cf
Vincent Robert <vincent.robert@nanego.com>
2019-07-22 09:43:12 +0200

    Remove debug output

--- lib/redmine_templates/menu_manager_patch.rb
+++ lib/redmine_templates/menu_manager_patch.rb
@@ -7,7 +7,6 @@ module Redmine
         if project.present? && project.issue_templates.present?
           Redmine::MenuManager.map :project_menu do |project_menu|
             project.issue_templates.includes(:tracker).order('trackers.position desc, issue_templates.template_title desc').each do |template|
-              puts "project_menu.find(:new_issue_template_#{template.id}) : #{project_menu.find("new_issue_template_#{template.id}".to_sym).inspect}"
               unless project_menu.find("new_issue_template_#{template.id}".to_sym)
                 project_menu.push "new_issue_template_#{template.id}".to_sym,
                                   { :controller => 'issues', :action => 'new', :template_id => template.id },
