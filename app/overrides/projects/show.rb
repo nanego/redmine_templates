762c2112bc061f0b8a6af82d76fc0d6c0b3c3272
Vincent Robert <vincent.robert@nanego.com>
2019-10-08 18:32:05 +0200

    Improve template cards UX - 2 per row

--- app/overrides/projects/show.rb
+++ app/overrides/projects/show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new  :virtual_path  => "projects/show",
                       :name          => "show-templates-on-overview",
-                      :insert_bottom => ".splitcontentright",
+                      :insert_top    => ".splitcontentright",
                       :partial       => "projects/overview_templates"
