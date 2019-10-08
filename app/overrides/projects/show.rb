32ca23a09534166d186fdb50931a6f6340f784eb
Vincent Robert <vincent.robert@nanego.com>
2019-10-08 18:17:39 +0200

    Update template cards UI

--- app/overrides/projects/show.rb
+++ app/overrides/projects/show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new  :virtual_path  => "projects/show",
                       :name          => "show-templates-on-overview",
-                      :insert_bottom       => ".splitcontentright",
+                      :insert_bottom => ".splitcontentright",
                       :partial       => "projects/overview_templates"
