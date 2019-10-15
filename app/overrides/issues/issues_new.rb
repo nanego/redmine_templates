e5e35b50dd0c89bd94b2ed05304c7005980f9ee6
Vincent Robert <vincent.robert@nanego.com>
2019-10-15 12:45:09 +0200

    No need to reload issue form anymore when applying template

--- app/overrides/issues/issues_new.rb
+++ app/overrides/issues/issues_new.rb
@@ -13,36 +13,22 @@ Deface::Override.new :virtual_path  => 'issues/new',
                                             id: "init_issue_template",
                                             "data-href": init_issue_template_path(project_id: @issue.project.id),
                                             class: "icon icon-copy pull-right" %>
-                                        <% end %>'
-
-Deface::Override.new :virtual_path  => 'issues/new',
-                     :name          => 'autocomplete-new-issue-from-url-template',
-                     :insert_after  => 'erb[loud]:contains("submit_tag l(:button_create_and_continue)")',
-                     :original => 'bcfa1ba4130d1a98d6dc7f126d897cfd5bda13fc' do
-  '
-  <%= f.hidden_field :issue_template_id %>
-  <script type="text/javascript">
-    <%= render(:partial => "issue_templates/load_select_js_functions", :handlers => [:erb], :formats => [:js]) %>
-    <% if @issue_template && !@issue_template.custom_form
-      @issue_template.increment!(:usage)
-      @issue.issue_template = @issue_template %>
-      <%= render(:partial => "issue_templates/load_update_functions", :handlers => [:erb], :formats => [:js]) %>
-      startUpdate();
-    <% end %>
-  </script>
-  '
-end
+                                        <% end %>
+                                        <%= f.hidden_field :issue_template_id %>
+                                        <script type="text/javascript">
+                                          <%= render(:partial => "issue_templates/load_select_js_functions", :handlers => [:erb], :formats => [:js]) %>
+                                          <% if @issue_template && !@issue_template.custom_form %>
+                                            <%= render(:partial => "issue_templates/load_update_functions", :handlers => [:erb], :formats => [:js]) %>
+                                            startUpdate();
+                                          <% end %>
+                                        </script>'
 
 # Custom form
 Deface::Override.new :virtual_path      => "issues/new",
                      :name              => "custom-form-from-templates",
                      :replace_contents  => "#all_attributes" do
   %(
-    <%
-      if @issue_template.present? && @issue_template.custom_form
-        @issue_template.increment!(:usage)
-        @issue.issue_template = @issue_template
-    %>
+    <% if @issue_template.present? && @issue_template.custom_form %>
       <%= render :partial => "issues/" + @issue_template.custom_form_path, :locals => {:f => f} %>
     <% else %>
       <%= render :partial => "issues/form", :locals => {:f => f} %>
--- lib/redmine_templates/issues_controller_patch.rb
+++ lib/redmine_templates/issues_controller_patch.rb
@@ -7,6 +7,13 @@ class IssuesController < ApplicationController
   def set_template
     if params[:template_id] && params[:template_id].to_i.to_s == params[:template_id]
       @issue_template = IssueTemplate.find_by_id(params[:template_id])
+      if @issue_template.present?
+        @issue.attributes = @issue_template.attributes.slice(*Issue.attribute_names)
+        @issue.project = @project
+        @issue.projects = @issue_template.secondary_projects
+        @issue.issue_template = @issue_template
+        @issue_template.increment!(:usage)
+      end
     end
   end
 
