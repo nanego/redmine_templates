ceeba64eeb96edafcbc5970591567647fb88f306
Vincent Robert <vincent.robert@nanego.com>
2019-10-15 16:39:54 +0200

    Use only safe attributes

--- lib/redmine_templates/issues_controller_patch.rb
+++ lib/redmine_templates/issues_controller_patch.rb
@@ -8,7 +8,7 @@ class IssuesController < ApplicationController
     if params[:template_id] && params[:template_id].to_i.to_s == params[:template_id]
       @issue_template = IssueTemplate.find_by_id(params[:template_id])
       if @issue_template.present?
-        @issue.attributes = @issue_template.attributes.slice(*Issue.attribute_names)
+        @issue.safe_attributes = @issue_template.attributes.slice(*Issue.attribute_names)
         @issue.project = @project
         @issue.projects = @issue_template.secondary_projects
         @issue.issue_template = @issue_template
