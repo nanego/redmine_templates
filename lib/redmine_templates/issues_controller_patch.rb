f4591a5fcf48afac99eb46b93a4fda770cf824c1
Vincent Robert <vincent.robert@nanego.com>
2019-11-14 15:21:00 +0100

    Authorize url params when applying template to new issue

--- lib/redmine_templates/issues_controller_patch.rb
+++ lib/redmine_templates/issues_controller_patch.rb
@@ -5,10 +5,13 @@ class IssuesController < ApplicationController
   before_action :set_template, :only => [:new]
 
   def set_template
+
+    permitted_params_override = params.require(:issue).to_unsafe_h
+
     if params[:template_id] && params[:template_id].to_i.to_s == params[:template_id]
       @issue_template = IssueTemplate.find_by_id(params[:template_id])
       if @issue_template.present?
-        @issue.safe_attributes = @issue_template.attributes.slice(*Issue.attribute_names)
+        @issue.safe_attributes = @issue_template.attributes.slice(*Issue.attribute_names).merge(permitted_params_override)
         @issue.project = @project
         @issue.projects = @issue_template.secondary_projects
         @issue.issue_template = @issue_template
