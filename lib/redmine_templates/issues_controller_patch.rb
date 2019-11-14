a3642a2224f60ec18251de714929e37356c4a04f
Vincent Robert <vincent.robert@nanego.com>
2019-11-14 16:25:51 +0100

    Issue params are not mandatory

--- lib/redmine_templates/issues_controller_patch.rb
+++ lib/redmine_templates/issues_controller_patch.rb
@@ -5,10 +5,8 @@ class IssuesController < ApplicationController
   before_action :set_template, :only => [:new]
 
   def set_template
-
-    permitted_params_override = params.require(:issue).to_unsafe_h
-
     if params[:template_id] && params[:template_id].to_i.to_s == params[:template_id]
+      permitted_params_override = params[:issue].present? ? params.require(:issue).to_unsafe_h : {}
       @issue_template = IssueTemplate.find_by_id(params[:template_id])
       if @issue_template.present?
         @issue.safe_attributes = @issue_template.attributes.slice(*Issue.attribute_names).merge(permitted_params_override)
