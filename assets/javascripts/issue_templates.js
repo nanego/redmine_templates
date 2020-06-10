56d838e0bbe9a5a20c772a8972b2729b6a1d861e
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-06-10 18:45:08 +0200

    Add split-description checkbox system to JS

--- assets/javascripts/issue_templates.js
+++ assets/javascripts/issue_templates.js
@@ -19,7 +19,13 @@ $(document).ready(function ($) {
     stimulus_application.register("template-form", class extends Stimulus.Controller {
 
         static get targets() {
-            return ["custom_form_radio_button", "custom_form_path_text_field", "custom_form", "standard_form"]
+            return [
+                "custom_form_radio_button",
+                "custom_form_path_text_field",
+                "custom_form",
+                "standard_form",
+                "split_description"
+            ]
         }
 
         connect() {
@@ -41,12 +47,35 @@ $(document).ready(function ($) {
                 this.custom_form_path_text_fieldTarget.parentNode.style.display = 'block';
                 this.custom_formTarget.style.display = 'block'
                 this.standard_formTarget.style.display = 'none'
+                this.split_descriptionTarget.style.display = 'none'
             } else {
                 this.custom_form_path_text_fieldTarget.parentNode.style.display = 'none';
                 this.custom_formTarget.style.display = 'none'
                 this.standard_formTarget.style.display = 'block'
+                this.split_descriptionTarget.style.display = 'block'
             }
         }
 
+    })
+
+    stimulus_application.register("split-description", class extends Stimulus.Controller {
+
+        static get targets() {
+            return ["split_description_checkbox", "split_description_number", "description_field"];
+        }
+
+        connect() {
+            this.toggleDescriptionSectionsField();
+        }
+
+        toggleDescriptionSectionsField() {
+            if (this.split_description_checkboxTarget.checked) {
+                this.split_description_numberTarget.style.display = "inline-block";
+                this.description_fieldTarget.style.display = "none";
+            } else {
+                this.split_description_numberTarget.style.display = "none";
+                this.description_fieldTarget.style.display = "block";
+            }
+        }
     })
 })();
