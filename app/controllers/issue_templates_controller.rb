a7d05a36269e4c5b605d3350812f5749d8d34d2e
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-07-13 18:41:18 +0200

    Add instruction to description_split system

--- app/controllers/issue_templates_controller.rb
+++ app/controllers/issue_templates_controller.rb
@@ -24,7 +24,7 @@ class IssueTemplatesController < ApplicationController
     @issue_template = IssueTemplate.new(custom_form: false)
     @priorities = IssuePriority.active
     @issue_template.project = @project if @project.present?
-    @issue_template.descriptions.build
+    @issue_template.descriptions << IssueTemplateDescriptionSection.new
   end
 
   def custom_form
@@ -37,7 +37,7 @@ class IssueTemplatesController < ApplicationController
   def edit
     @issue_template = IssueTemplate.find(params[:id])
     @priorities = IssuePriority.active
-    @issue_template.descriptions.build if @issue_template.descriptions.empty?
+    @issue_template.descriptions << IssueTemplateDescriptionSection.new if @issue_template.descriptions.empty?
   end
 
   def create
@@ -59,6 +59,8 @@ class IssueTemplatesController < ApplicationController
       end
     else
       @priorities = IssuePriority.active
+      @issue_template.descriptions << IssueTemplateDescriptionSection.new
+
       respond_to do |format|
         format.html { render :action => :new }
       end
