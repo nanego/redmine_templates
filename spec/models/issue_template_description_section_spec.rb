ce061e329601b983e5d84ecbc9b17569be1b6cfe
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-07-15 16:59:58 +0200

    Dynamize Wikitoolbar links + clean code

--- spec/models/issue_template_description_section_spec.rb
+++ spec/models/issue_template_description_section_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "IssueTemplateDescriptionSection" do
+describe IssueTemplateDescriptionSection do
   it "belongs to issue_template" do
     t = IssueTemplateDescriptionSection.reflect_on_association(:issue_template)
     expect(t.macro).to eq(:belongs_to)
