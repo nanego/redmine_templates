ab9a8b1abb94527248d24bc99de0d40b70d503e8
Vincent Robert <vincent.robert@nanego.com>
2020-08-18 12:01:48 +0200

    Remove legacy code

--- app/helpers/issue_templates_helper.rb
+++ app/helpers/issue_templates_helper.rb
@@ -1,40 +1,4 @@
 module IssueTemplatesHelper
-  def grouped_templates_for_select(grouped_options, project, selected_key = nil, prompt = nil)
-    body = ''
-    body << content_tag(:option, prompt, { :value => "" }, true) if prompt
-
-    grouped_options = grouped_options.sort if grouped_options.is_a?(Hash)
-
-    grouped_options.each do |group|
-      body << templates_options_for_select(group[0], group[1], project, selected_key)
-    end
-
-    if User.current.admin? || User.current.allowed_to?(:create_issue_templates, project)
-      body << content_tag(:option, "-----------", { :value => "", :disabled => 'disabled' }, true)
-      body << content_tag(:option, l("show_templates"), { :value => issue_templates_path(project_id: project.id) }, true)
-    end
-
-    body.html_safe
-  end
-
-  def templates_options_for_select(tracker, container, project, selected = nil )
-    return container if String === container
-
-    selected, disabled = extract_selected_and_disabled(selected).map do | r |
-      Array.wrap(r).map { |item| item.to_s }
-    end
-
-    container.map do |element|
-      html_attributes = option_html_attributes(element)
-      text = "#{element.template_title.first(40)}..." if element.template_title.size > 40
-      text ||= element.template_title
-      value = element.id
-      selected_attribute = ' selected="selected"' if option_value_selected?(value, selected)
-      disabled_attribute = ' disabled="disabled"' if disabled && option_value_selected?(value, disabled)
-      %(<option value="#{ERB::Util.html_escape(new_project_issue_url(project_id: project.identifier, template_id: value))}"#{selected_attribute}#{disabled_attribute}#{html_attributes}>[#{ERB::Util.html_escape(tracker)}] #{ERB::Util.html_escape(text)}</option>)
-    end.join("\n").html_safe
-
-  end
 
   def project_tree_options(projects, options = {})
     s = ''
