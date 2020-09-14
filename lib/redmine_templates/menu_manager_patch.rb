9cce841e0f2c4c914eb0a44a81f40637346a22b1
Vincent Robert <vincent.robert@nanego.com>
2020-09-14 14:31:30 +0200

    Fix menu with disabled templates

--- lib/redmine_templates/menu_manager_patch.rb
+++ lib/redmine_templates/menu_manager_patch.rb
@@ -3,19 +3,19 @@ module PluginTemplates
     def render_menu(menu, project = nil)
 
       if project.present? &&
-          project.issue_templates.active.present? &&
-          (Issue.allowed_target_trackers(project) & project.issue_templates.active.map(&:tracker)).any?
+          project.issue_templates.present? &&
+          (Issue.allowed_target_trackers(project) & project.issue_templates.map(&:tracker)).any?
         Redmine::MenuManager.map :project_menu do |project_menu|
-          project.issue_templates.active.includes(:tracker).order('trackers.position desc, issue_templates.template_title desc').each do |template|
+          project.issue_templates.includes(:tracker).order('trackers.position desc, issue_templates.template_title desc').each do |template|
             unless project_menu.find("new_issue_template_#{template.id}".to_sym)
               project_menu.push "new_issue_template_#{template.id}".to_sym,
                                 {:controller => 'issues', :action => 'new', :template_id => template.id},
                                 :param => :project_id,
-                                :caption => Proc.new {|p| template.reload; template.title_with_tracker},
+                                :caption => Proc.new { template.reload; template.title_with_tracker },
                                 :html => {:accesskey => Redmine::AccessKeys.key_for(:new_issue)},
-                                :if => Proc.new {|project|
-                                  Issue.allowed_target_trackers(project).include?(template.tracker) &&
-                                      template.template_projects.include?(project)},
+                                :if => Proc.new { |project| template.reload; template.template_enabled &&
+                                    Issue.allowed_target_trackers(project).include?(template.tracker) &&
+                                    template.template_projects.include?(project) },
                                 :permission => :add_issues,
                                 :parent => :new_issue,
                                 :first => true
