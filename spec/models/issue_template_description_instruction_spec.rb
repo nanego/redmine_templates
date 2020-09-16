a91acb7a87105c3aba6d55091b183d68cc6d8e7e
Vincent Robert <vincent.robert@nanego.com>
2020-09-16 17:13:47 +0200

    Fix models specs

--- spec/models/issue_template_description_instruction_spec.rb
+++ spec/models/issue_template_description_instruction_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe IssueTemplateDescriptionInstruction do
+describe IssueTemplateDescription do
   it "belongs to issue_template" do
     t = IssueTemplateDescriptionInstruction.reflect_on_association(:issue_template)
     expect(t.macro).to eq(:belongs_to)
--- spec/models/issue_template_description_section_spec.rb
+++ spec/models/issue_template_description_section_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe IssueTemplateDescriptionSection do
+describe IssueTemplateDescription do
   it "belongs to issue_template" do
     t = IssueTemplateDescriptionSection.reflect_on_association(:issue_template)
     expect(t.macro).to eq(:belongs_to)
