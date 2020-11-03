34fd81fd58f064dcc0df365475867e8f0a297610
Vincent Robert <vincent.robert@nanego.com>
2020-11-03 14:37:54 +0100

    Double test db migration

--- db/migrate/20201015152853_add_typology_id_to_issue_templates.rb
+++ db/migrate/20201015152853_add_typology_id_to_issue_templates.rb
@@ -1,6 +1,6 @@
 class AddTypologyIdToIssueTemplates < ActiveRecord::Migration[5.2]
   def self.up
-    add_column :issue_templates, :typology_id, :integer, :default => nil
+    add_column :issue_templates, :typology_id, :integer, :default => nil unless ActiveRecord::Base.connection.column_exists?('issue_templates', 'typology_id')
   end
 
   def self.down
