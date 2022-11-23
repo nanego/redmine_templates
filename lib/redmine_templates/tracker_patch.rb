b5a9330c492726bd9d787bd39361624bdb6acdc0
Yalaeddin <y.alaeddin@adacis.net>
2022-11-23 14:37:12 +0100

    Mettre à jour les tables de relations lors de la suppression en cascade

---
+++ lib/redmine_templates/tracker_patch.rb
@@ -0,0 +1,11 @@
+require_dependency 'tracker'
+
+class Tracker
+
+  has_many :issue_templates
+  before_destroy :set_default_tracker_id_in_issue_templates
+
+  def set_default_tracker_id_in_issue_templates
+    IssueTemplate.where('tracker_id = ?', id).update_all('tracker_id = 0')    
+  end
+end
---
+++ lib/redmine_templates/typology_patch.rb
@@ -0,0 +1,5 @@
+require_dependency "enumeration"
+
+class Typology < Enumeration   
+  has_many :issue_templates,  :dependent => :nullify
+end
