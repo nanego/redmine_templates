3b9fb44f447a6eb2767b4e9ed26a33cedbec142f
Yalaeddin <y.alaeddin@adacis.net>
2023-07-17 14:20:16 +0200

    Ajouter l'état "disabled" pour chaque valeur d'une liste de valeur

---
+++ spec/system/issue_templates_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+require "active_support/testing/assertions"
+
+def log_user(login, password)
+  visit '/my/page'
+  expect(current_path).to eq '/login'
+
+  if Redmine::Plugin.installed?(:redmine_scn)
+    click_on("ou s'authentifier par login / mot de passe")
+  end
+
+  within('#login-form form') do
+    fill_in 'username', with: login
+    fill_in 'password', with: password
+    find('input[name=login]').click
+  end
+  expect(current_path).to eq '/my/page'
+end
+
+RSpec.describe "creating issues with templates", type: :system do
+  include ActiveSupport::Testing::Assertions
+  include IssuesHelper
+
+  fixtures :projects, :users, :issues,
+           :issue_templates, :issue_template_projects, :issue_template_section_groups, :issue_template_sections
+ 
+  before do
+    log_user('admin', 'admin')
+  end
+
+  describe "test read only values in the list of values of section template" do
+    it "Save the read only values in the column empty_data of the table issue_template_sections" do
+      visit edit_issue_template_path(1)
+ 
+      expect(page).to have_selector('#issue_template_split_description')
+      
+      find("#issue_template_split_description").click
+      find(".icon-add").click
+      find(".icon-list").click
+      find_by_id("new_section").find("option[value='7']").click #['Nouvelle liste de valeurs', 7]
+
+      # add section group
+      find('button[data-action="split-description#addSection"]').click
+      # click on Show the details    
+      all('a[data-action="description-item-form#expand_collapse"]')[1].click
+      # select list of values
+      fill_in 'issue_template_section_groups_attributes__id_group_section__sections_attributes_0_title', with: 'list test'
+
+      array_values = ['val_1', 'val_2', 'val_3']
+      # **** add the first value  **** 
+      find(".add_possible_value").click
+
+      # fill the informations
+      find("#checked").click
+      fill_in "value", with: 'val_1'
+      find("#read_only").click
+
+      # **** add the second value  **** 
+      find(".add_possible_value").click
+      all('#checked')[1].click 
+      all("#value")[1].fill_in with: 'val_2'
+      
+      # **** add the third value  **** 
+      find(".add_possible_value").click
+      all('#value')[2].fill_in with: 'val_3'
+      all('#read_only')[2].click
+ 
+      # save the template
+      find('input[name=commit]').click
+      
+      section_test = IssueTemplate.find(1).section_groups[0].sections[0]
+      expect(section_test.empty_value).to eq("#{array_values[0]};#{array_values[2]}")
+      expect(section_test.placeholder).to eq("#{array_values[0]};#{array_values[1]}")
+ 
+    end   
+  end
+  
+end
