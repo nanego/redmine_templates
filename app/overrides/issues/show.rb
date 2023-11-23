761e8c67183d62887f463bc5287be9bac765d1d7
Yalaeddin <y.alaeddin@adacis.net>
2023-11-23 13:23:25 +0100

    Afficher les consignes dans la page issue/show

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -12,3 +12,20 @@ Deface::Override.new :virtual_path  => 'issues/show',
                                           end
                                         end %>
                                         }
+Deface::Override.new :virtual_path  => 'issues/show',
+                      :name          => 'show-issue-template-section-instruction',
+                      :insert_bottom => 'div.description',
+                      :text          => %{
+                                          <% @issue.issue_template.section_groups.each do |group| %>
+                                            <%  group.sections.each do |section| %>
+                                              <% # section.show_toolbar means that Show in the generated issue %>
+                                              <% if section.type == "IssueTemplateSectionInstruction" && section.show_toolbar %>
+                                                <div style="height: 90px;margin-bottom: 50px;margin-top: 50px;">
+                                                  <div class="instruction instruction-<%= section.instruction_type %>" style="height: 100%;padding-left: 50px;padding-top: 20px;">
+                                                    <%= textilizable section.text %>
+                                                  </div>
+                                                </div>
+                                              <% end %>
+                                            <% end %>
+                                          <% end %>
+                                          }
