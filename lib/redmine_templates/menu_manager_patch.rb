55ca75aba8a2c51b946acb241e689ff2f585bd70
Vincent Robert <vincent.robert@nanego.com>
2024-01-10 15:28:11 +0100

    Resolve link issues associated with shared MenuItems across multiple projects

--- lib/redmine_templates/menu_manager_patch.rb
+++ lib/redmine_templates/menu_manager_patch.rb
@@ -2,14 +2,15 @@ module RedmineTemplates
   module MenuManagerPatch
     def render_menu(menu, current_project = nil)
 
-      if (current_project.present? &&
+      if current_project.present? &&
         current_project.issue_templates.present? &&
-        (Issue.allowed_target_trackers(current_project) & current_project.issue_templates.map(&:tracker)).any?)
+        (Issue.allowed_target_trackers(current_project) & current_project.issue_templates.map(&:tracker)).any?
         Redmine::MenuManager.map :project_menu do |project_menu|
           current_project.issue_templates.includes(:tracker).reorder('trackers.position asc, issue_templates.template_title asc').each do |template|
-            unless project_menu.find("new_issue_template_#{template.id}".to_sym)
-              project_menu.push "new_issue_template_#{template.id}".to_sym,
-                                new_project_issue_path(project: current_project, template_id: template.id),
+            menu_item_name = "new_issue_template_#{template.id}".to_sym
+            unless project_menu.find(menu_item_name)
+              project_menu.push menu_item_name,
+                                { :controller => 'issues', :action => 'new', :template_id => template.id },
                                 :param => :project_id,
                                 :caption => Proc.new {
                                   template.reload unless template.has_been_deleted?
