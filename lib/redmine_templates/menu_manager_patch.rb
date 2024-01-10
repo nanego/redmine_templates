cde9d6e0717ab40eef6992892205dbf7d33ec177
Vincent Robert <vincent.robert@nanego.com>
2024-01-10 11:56:03 +0100

    Fix params of new_projet_issue link in ProjectMenu

--- lib/redmine_templates/menu_manager_patch.rb
+++ lib/redmine_templates/menu_manager_patch.rb
@@ -1,29 +1,29 @@
 module RedmineTemplates
   module MenuManagerPatch
-    def render_menu(menu, project = nil)
+    def render_menu(menu, current_project = nil)
 
-      if project.present? &&
-        project.issue_templates.present? &&
-        (Issue.allowed_target_trackers(project) & project.issue_templates.map(&:tracker)).any?
+      if (current_project.present? &&
+        current_project.issue_templates.present? &&
+        (Issue.allowed_target_trackers(current_project) & current_project.issue_templates.map(&:tracker)).any?)
         Redmine::MenuManager.map :project_menu do |project_menu|
-          project.issue_templates.includes(:tracker).reorder('trackers.position asc, issue_templates.template_title asc').each do |template|
+          current_project.issue_templates.includes(:tracker).reorder('trackers.position asc, issue_templates.template_title asc').each do |template|
             unless project_menu.find("new_issue_template_#{template.id}".to_sym)
               project_menu.push "new_issue_template_#{template.id}".to_sym,
-                                new_project_issue_path(project_id: project.identifier, template_id: template.id),
+                                new_project_issue_path(project: current_project, template_id: template.id),
                                 :param => :project_id,
                                 :caption => Proc.new {
                                   template.reload unless template.has_been_deleted?
                                   template.title_with_tracker
                                 },
                                 :html => { :accesskey => Redmine::AccessKeys.key_for(:new_issue) },
-                                :if => Proc.new { |project|
+                                :if => Proc.new { |current_project|
                                   if template.has_been_deleted?
                                     false
                                   else
                                     template.reload
                                     template.template_enabled &&
-                                      Issue.allowed_target_trackers(project).include?(template.tracker) &&
-                                      template.template_projects.include?(project)
+                                      Issue.allowed_target_trackers(current_project).include?(template.tracker) &&
+                                      template.template_projects.include?(current_project)
                                   end
                                 },
                                 :permission => :add_issues,
