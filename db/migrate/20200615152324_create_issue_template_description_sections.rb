a08c579616c403392e3cd17faa4d7683ac3e03b4
Vincent <contact@vincent-robert.com>
2026-03-17 16:50:45 +0100

    Fix FK type mismatch in migrations

--- db/migrate/20200615152324_create_issue_template_description_sections.rb
+++ db/migrate/20200615152324_create_issue_template_description_sections.rb
@@ -4,7 +4,7 @@ class CreateIssueTemplateDescriptionSections < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :description
       t.text :text
-      t.references :issue_template, :foreign_key => { index: true }
+      t.references :issue_template, type: :integer, :foreign_key => { index: true }
     end unless ActiveRecord::Base.connection.table_exists? 'issue_template_description_sections'
   end
 end
