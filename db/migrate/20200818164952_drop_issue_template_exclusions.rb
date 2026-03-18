913a7cb1c74de19392d156c35e448ed47a44d30a
Vincent <contact@vincent-robert.com>
2026-03-18 07:31:12 +0100

    Fix migration rollback

--- db/migrate/20200818164952_drop_issue_template_exclusions.rb
+++ db/migrate/20200818164952_drop_issue_template_exclusions.rb
@@ -1,5 +1,16 @@
 class DropIssueTemplateExclusions < ActiveRecord::Migration[4.2]
-  def change
+  def up
     drop_table :issue_template_exclusions
+  end
+
+  def down
+    create_table :issue_template_exclusions, id: false do |t|
+      t.belongs_to :issue_template
+      t.belongs_to :project
+    end
+    add_index :issue_template_exclusions, ["project_id", "issue_template_id"],
+              name: :unique_issue_template_exclusion_per_project, unique: true
+    add_index :issue_template_exclusions, :issue_template_id
+    add_index :issue_template_exclusions, :project_id
   end
 end
